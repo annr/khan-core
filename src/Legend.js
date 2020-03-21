631e4c47dfc52511fb4672fe0a7f3e312ebeb103
Ann Robson <nosbora@gmail.com>
2020-03-21 13:48:43 -0700

    implement toggle legend, and extract some shared styles. make Legend a stateless component

--- src/Legend.js
+++ src/Legend.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import { scaleLinear } from "d3-scale";
 import "./Legend.css"
 
@@ -11,102 +11,91 @@ const ENABLED = scaleLinear().domain([1, 6])
 const RELATED = scaleLinear().domain([1, 6])
     .range(["#9db8e0", "white"]);
 
-export default class Legend extends React.Component {
-
-    // constructor(props) {
-    //     super(props)
-    //     this.createLegend = this.createLegend.bind(this)
-    // }
-    // componentDidMount() {
-    //     this.createLegend()
-    // }
-    // componentDidUpdate() {
-    //     this.createLegend()
-    // }
-    // createLegend() {
-
-    // }
-
-    renderSelected() {
+export default function Legend() {
+    const [isOpen, toggle] = useState(false);
+    function renderSelected() {
         return (
             <svg height="36" width="36" className="legendItem">
-                <circle r="12" cx="18" cy="18" class="selected-node-example"></circle>
-                <circle r="15" cx="18" cy="18" class="selectedRing-example"></circle>
+                <circle r="12" cx="18" cy="18" className="selected-node-example"></circle>
+                <circle r="15" cx="18" cy="18" className="selectedRing-example"></circle>
             </svg>
         );
     }
 
-    renderRelated() {
+    function renderRelated() {
         return (
             <svg height="36" width="36" className="legendItem">
-                <circle r="15" cx="18" cy="18" class="non-directional-example" fill={RELATED(1)}></circle>
+                <circle r="15" cx="18" cy="18" className="non-directional-example" fill={RELATED(1)}></circle>
             </svg>
         );
     }
 
-    renderPrerequisite() {
+    function renderPrerequisite() {
         return (
             <svg height="36" width="36" className="legendItem">
-                <circle r="15" cx="18" cy="18" class="prerequisite-example" fill={PREREQS(1)}></circle>
+                <circle r="15" cx="18" cy="18" className="prerequisite-example" fill={PREREQS(1)}></circle>
             </svg>
         );
     }
 
-    renderEnabled() {
+    function renderEnabled() {
         return (
             <svg height="40" width="40" className="legendItem">
-                <circle r="15" cx="18" cy="18" class="enabled-example" fill={ENABLED(1)}></circle>
+                <circle r="15" cx="18" cy="18" className="enabled-example" fill={ENABLED(1)}></circle>
             </svg>
         );
     }
 
-    renderMajor() {
+    function renderMajor() {
         return (
             <svg height="36" width="36" className="legendItem">
-                <circle r="15" cx="18" cy="18" class="cluster-example"></circle>
-                <circle r="15" cx="18" cy="18" class="major-example"></circle>
+                <circle r="15" cx="18" cy="18" className="cluster-example"></circle>
+                <circle r="15" cx="18" cy="18" className="major-example"></circle>
             </svg>
         );
     }
 
-    renderAdditional() {
+    function renderAdditional() {
         return (
             <svg height="36" width="36" className="legendItem">
-                <circle r="15" cx="18" cy="18" class="cluster-example"></circle>
-                <circle r="15" cx="18" cy="18" class="additional-example"></circle>
+                <circle r="15" cx="18" cy="18" className="cluster-example"></circle>
+                <circle r="15" cx="18" cy="18" className="additional-example"></circle>
             </svg>
         );
     }
 
-    renderSupporting() {
+    function renderSupporting() {
         return (
             <svg height="36" width="36" className="legendItem">
-                <circle r="15" cx="18" cy="18" class="cluster-example"></circle>
-                <circle r="15" cx="18" cy="18" class="supporting-example"></circle>
+                <circle r="15" cx="18" cy="18" className="cluster-example"></circle>
+                <circle r="15" cx="18" cy="18" className="supporting-example"></circle>
             </svg>
         );
     }
 
-    render() {
-        return (
-            <div id="legend">
-                <dl>
-                    <dt>
-                        Legend
-                    </dt>
-                    <dl className="standardsList">
-                        <dd>{this.renderSelected()} selected standard</dd>
-                        <dd>{this.renderRelated()} related standard</dd>
-                        <dd>{this.renderPrerequisite()} prerequisite standard</dd>
-                        <dd>{this.renderEnabled()} enabled standard</dd>
-                    </dl>
-                    <dl className="clustersList">
-                        <dd>{this.renderMajor()} major cluster</dd>
-                        <dd>{this.renderAdditional()} additional cluster</dd>
-                        <dd>{this.renderSupporting()} supporting cluster</dd>
-                    </dl>
+    return (
+        <div id="legend" className={`panel ${isOpen ? "legend-open" : "legend-closed"}`}>
+            <button type="button" aria-label="Toggle side bar" id="legendToggleIcon" className={`toggle-icon ${isOpen ? "legendOpen" : "legendClosed"}`} onClick={() => toggle(!isOpen)}>
+                <svg width="20" height="30" xmlns="http://www.w3.org/2000/svg" version="1.1">
+                    <polyline points="5 5 15 15 5 25" stroke="rgba(0,0,0,0.4)" strokeWidth="3" strokeLinecap="butt" fill="none" strokeLinejoin="round"></polyline>
+                </svg>
+            </button>
+            <dl>
+                <dt>
+                    Legend
+                </dt>
+                <dl className="standardsList">
+                    <dd>{renderSelected()} selected standard</dd>
+                    <dd>{renderRelated()} related standard</dd>
+                    <dd>{renderPrerequisite()} prerequisite standard</dd>
+                    <dd>{renderEnabled()} enabled standard</dd>
                 </dl>
-            </div>
-        );
-    }
+                <dl className="clustersList">
+                    <dd>{renderMajor()} major cluster</dd>
+                    <dd>{renderAdditional()} additional cluster</dd>
+                    <dd>{renderSupporting()} supporting cluster</dd>
+                </dl>
+            </dl>
+        </div>
+    );
 }
--- src/SidePanel.js
+++ src/SidePanel.js
@@ -5,14 +5,12 @@ import SidePanelContent from "./SidePanelContent";
 export default function SidePanel() {
     const [isOpen, toggle] = useState(true);
     return (
-        <div id="side-panel" className={isOpen ? "side-panel-open" : "side-panel-closed"} >
-            <div className="toggleWrapper">
-                <button type="button" aria-label="Toggle side bar" id="collapseIcon" className={isOpen ? "open" : "closed"} onClick={() => toggle(!isOpen)}>
-                    <svg width="20" height="30" xmlns="http://www.w3.org/2000/svg" version="1.1">
-                        <polyline points="5 5 15 15 5 25" stroke="rgba(0,0,0,0.5)" strokeWidth="3" strokeLinecap="butt" fill="none" strokeLinejoin="round"></polyline>
-                    </svg>
-                </button>
-            </div>
+        <div id="side-panel" className={`panel ${isOpen ? "side-panel-open" : "side-panel-closed"}`} >
+            <button type="button" aria-label="Toggle side bar" id="sidePanelToggleIcon" className={`toggle-icon ${isOpen ? "open" : "closed"}`} onClick={() => toggle(!isOpen)}>
+                <svg width="20" height="30" xmlns="http://www.w3.org/2000/svg" version="1.1">
+                    <polyline points="5 5 15 15 5 25" stroke="rgba(0,0,0,0.4)" strokeWidth="3" strokeLinecap="butt" fill="none" strokeLinejoin="round"></polyline>
+                </svg>
+            </button>
             <SidePanelContent />
         </div >
     );
